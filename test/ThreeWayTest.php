069fbbebc3a69d4b08218461f357176f0714cdde
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-06 21:22:53 +0200

    test: Modernize test suite for phpunit 11

--- test/ThreeWayTest.php
+++ test/ThreeWayTest.php
@@ -7,11 +7,15 @@
  * @subpackage UnitTests
  */
 namespace Horde\Text\Diff\Test;
-use Horde\Test\TestCase as TestCase;
+
 use Horde\Text\Diff\ThreeWay;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
 /**
  * @requires PHP >= 8.1
  */
+#[CoversClass(ThreeWay::class)]
 class ThreeWayTest extends TestCase
 {
     protected $_lines = array();
